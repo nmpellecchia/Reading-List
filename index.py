25577337d7eb428f473dd376ea97405fdc5b33a4
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-17 18:03:34 -0300

    Added way to get all books

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, json, jsonify
+from handleData import get_all_books
 
 app = Flask(__name__)
 
@@ -9,6 +10,13 @@ def hello():
 @app.route("/edit")
 def hello():
     return render_template("bookEditor.html")
+
+####### Manipulate data #####
+
+@app.route("/book-data")
+def get_data():
+    all_books = get_all_books()
+    return jsonify(all_books), 201
 
 if __name__ == "__main__":
     app.run()
