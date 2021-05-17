ffd067f035af28ecbf547eb3acc7ef5d738d7687
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-17 16:14:57 -0300

    Added a book editor page
    Toadd, edit or delete the books!

--- index.py
+++ index.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route("/edit")
+def hello():
+    return render_template("bookEditor.html")
 
 if __name__ == "__main__":
     app.run()
