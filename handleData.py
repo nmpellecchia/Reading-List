9de656746f5a70fbfc7b152951371c5e3a8ee645
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-18 15:35:49 -0300

    add book and delete book fn

--- handleData.py
+++ handleData.py
@@ -1,3 +1,5 @@
+from flask import json
+
 
 def get_all_books():
     book_list = {}
@@ -20,3 +22,26 @@ def get_all_books():
     print(books)
     print(book_list)
     return book_list
+
+##################
+
+def add_book(book):
+    str_book = json.dumps(book)
+    no_quote_book = str_book.strip('"')
+    with open('book-list.csv', 'a') as reading_list:
+        reading_list.write(f"{no_quote_book}\n")
+    return 'OK'
+
+##################
+
+def delete_book(book):
+    book_to_delete = (json.dumps(book)).strip('"')
+    # copy ALL the lines (This is not efficient, that's why we don't store things like this)
+    with open('book-list.csv', 'r') as complete_list:
+        lines = complete_list.readlines()
+    # now read every line and remove the one that matches
+    with open('book-list.csv', 'w') as reading_list:
+        for line in lines:
+            if book_to_delete not in line.strip("\n"):
+                print("We'll rewrite this line: " + line)
+                reading_list.write(line)
