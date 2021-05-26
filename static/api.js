45fe5e961f04e2565f07698a2ad7bdf905f953a3
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-26 17:50:25 -0300

    changed to jquery
    To handle more easily the API and DOM. The function convertToJSON now is not needed anymore.

--- static/api.js
+++ static/api.js
@@ -1,18 +1,21 @@
 async function getAllBooks() {
-  const JSONBookList = await fetch('/book-data').then(resp => {
+  /* const JSONBookList = await fetch('/book-data').then(resp => {
     if (resp.status == 201) {
       const r = convertToJSON(resp);
       return r;
     } else {
       return showError();
     }
+  }); */
+  const JSONBookList = await $.get('/book-data', resp => {
+    return resp;
   });
   console.log(JSONBookList.response);
   return JSONBookList.response;
 }
 
-function convertToJSON(data) {
+/* function convertToJSON(data) {
   return data.json();
-}
+} */
 
 export { getAllBooks };
