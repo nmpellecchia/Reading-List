7d7c4c163a1f4c7d8dcb85fd9deef8683f2a018d
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-28 17:52:20 -0300

    Changed books categories order

--- static/api.js
+++ static/api.js
@@ -10,8 +10,27 @@ async function getAllBooks() {
   const JSONBookList = await $.get('/book-data', resp => {
     return resp;
   });
-  console.log(JSONBookList.response);
-  return JSONBookList.response;
+  /* I don't like the order the data is sent. So I will change it */
+  const orderedList = orderList(JSONBookList.response);
+  return orderedList;
+}
+
+/* This would have to be refactored manually if keys are added or deleted later */
+/* But for this simple case I think is more than enough */
+function orderList(listToOrder) {
+  const orderedList = [];
+  /* Iterate the array and order the keys inside the objects */
+  listToOrder.forEach(object => {
+    /* Create a new object  */
+    const orderedObject = {
+      title: object.title,
+      author: object.author,
+      status: object.status,
+      rating: object.rating,
+    };
+    orderedList.push(orderedObject);
+  });
+  return orderedList;
 }
 
 /* function convertToJSON(data) {
