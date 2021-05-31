5642118d76db5f0d874ec8be9a9eb57cc733a9f7
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-05-31 17:53:09 -0300

    Update editor.js

--- static/editor.js
+++ static/editor.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
   function areInputsRight() {
-    const $inputs = document.querySelectorAll('.user-selector');
-    $inputs.forEach(input => {
-      if (input.id.contains('author')) {
-        /*  */
-      } else if (input.id.contains('title')) {
-        /*  */
-      } else if (input.id.contains('author')) {
-        /*  */
-      } else if (input.id.contains('status')) {
-        /*  */
-      } else if (input.id.contains('rating')) {
-        /*  */
-      }
+    const $selectors = document.querySelectorAll('.user-selector');
+    $selectors.forEach(selector => {
+      /* Could use tagName. But nodeName is less prone to errors */
+      /* switch (selector.nodeName) {
+        case 'SELECT':
+          console.log('Im a select element!' + selector.nodeName);
+          console.log('Im a select element!' + selector.value);
+          isSelectValid(selector.value);
+          break;
+
+        default:
+          // Should be an input
+          console.log('Im an input!' + selector.nodeName);
+          console.log('Im an input!' + selector.value);
+          isInputValid(selector.value);
+          break;
+      } */
+      console.log(selector.value);
+      isSelectorValid(selector.value);
     });
+  }
+
+  function isSelectorValid(value) {
+    value ? console.log(true) : console.log(false);
   }
 
   /* Event Listener */
